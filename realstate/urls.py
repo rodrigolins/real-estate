71f94ae25723e937e78d5288bc63be41154923fa
Rodrigo Lins <lins.oliveira@gmail.com>
2015-05-24 23:26:15 +0200

    added debug toolbar to project

--- realstate/urls.py
+++ realstate/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('apps.core.urls', namespace='core')),
     url(r'^landlord/', include('apps.landlord.urls', namespace='landlord')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
