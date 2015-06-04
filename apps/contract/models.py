1ea0653994916f094ab939b10f3327f29ee7eabd
Rodrigo Lins <lins.oliveira@gmail.com>
2015-06-04 19:38:04 +0200

    Test commit

--- apps/contract/models.py
+++ apps/contract/models.py
@@ -12,14 +12,7 @@ class Contract(models.Model):
         ('OP', 'Operative'),
         ('CN', 'Canceled'),
     )
-    property = models.ForeignKey(Property)
-    tenant = models.ForeignKey(Tenant)
-    duration = models.IntegerField(help_text='In months')
-    start_date = models.DateField()  # when contract start
-    finished_date = models.DateField(blank=True, null=True)  # when contract actually ended
-    status = models.CharField(max_length=2, choices=STATUS)
-    finished_reason = models.TextField(blank=True, null=True)
-    due_date = models.IntegerField()
+
 
     def __str__(self):
         return("%s %s" % (self.tenant.name, self.property.address))
